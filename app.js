2e021a5a9624c8d615eace53daf3d94b37411fad
Franco <francoalvarez.099@gmail.com>
2022-03-27 23:47:13 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
+
+punto.foreach( (cadaPunto , i) => {
+    punto[i].addEventListener('click',() =>{
+
+        let posicion = i
+        let operacion = posicion * -50
+
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        punto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+
+    }) 
+});
